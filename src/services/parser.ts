36b78eff0a66e81ff31575047e588747960357d7
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-06-04 17:33:24 +0400

    Create New Item fetching mechanism for RSS Parser

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -3,23 +3,46 @@
  */
 
 import Parser from 'rss-parser';
+import { Document } from 'arangojs/lib/cjs/util/types';
 
 /* Constants */
 const PARSER = new Parser();
 
 /* Exports */
-export default { parseURL };
+export default { parseURL, getNewItems };
 
 /* Module Functions */
+async function getNewItems(
+  feed: Document<Repo.Feed>,
+): Promise<Repo.Item[] | null> {
+  const parsedFeed = await parseURL(feed.link);
+  const items: Repo.Item[] = getStandardizedItems(
+    parsedFeed.items,
+  ) as Repo.Item[];
+
+  const currentLastItemGuid = items[0].guid;
+  if (feed.lastItemGuid === currentLastItemGuid) return null;
+
+  const lastItemIndex: number = items.findIndex(
+    (item: Repo.Item): boolean => item.guid === feed.lastItemGuid,
+  );
+
+  const newItems: Repo.Item[] = items.slice(0, lastItemIndex);
+  return newItems;
+}
+
 async function parseURL(url: string): Promise<Parser.Output> {
   try {
     const feed = await PARSER.parseURL(url);
-    return feed;
+    const items = getStandardizedItems(feed.items);
+
+    return { ...feed, items };
   } catch (error) {
     throw buildError(error);
   }
 }
 
+/* Private Functions */
 function buildError(error: Error): Error & { statusCode?: number } {
   // Standardizing possible error messages
   // Error messages taken from: https://github.com/rbren/rss-parser/blob/master/lib/parser.js
@@ -35,4 +58,18 @@ function buildError(error: Error): Error & { statusCode?: number } {
   }
 
   return newError;
+}
+
+function getStandardizedItems(items: Parser.Item[]): Parser.Item[] {
+  const item = items[0];
+
+  if (item.guid) return items;
+
+  const itemsWithGuid = items.map((item: Parser.Item) => ({
+    ...item,
+    id: undefined,
+    guid: item.id,
+  }));
+
+  return itemsWithGuid;
 }
